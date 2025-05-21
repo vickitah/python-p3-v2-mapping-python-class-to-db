63731b524856f2eadb0e357d15f6da33a906a00d
victor <victor.mutuma@student.moringaschool.com>
2025-05-21 13:38:46 +0300

    save

--- lib/debug.py
+++ lib/debug.py
@@ -1,9 +1,30 @@
-#!/usr/bin/env python3
-#lib/testing/debug.py
-
-from __init__ import CONN, CURSOR
 from department import Department
 
-import ipdb
+# Reset the table
+Department.drop_table()
+Department.create_table()
 
-ipdb.set_trace()
+# Create and save departments
+dept1 = Department("Engineering")
+dept1.save()
+print(dept1)  # Should print with an ID
+
+dept2 = Department.create("Marketing")
+print(dept2)
+
+# Update a department
+dept1.name = "Software Engineering"
+dept1.save()
+print(dept1)
+
+# Find by id
+found = Department.find_by_id(dept1.id)
+print(found)
+
+# Get all departments
+all_depts = Department.get_all()
+print(all_depts)
+
+# Delete
+dept2.delete()
+print(Department.get_all())
--- lib/department.py
+++ lib/department.py
@@ -1,12 +1,66 @@
-from __init__ import CURSOR, CONN
-
+from __init__ import CONN, CURSOR
 
 class Department:
-
     def __init__(self, name, location, id=None):
         self.id = id
         self.name = name
         self.location = location
 
     def __repr__(self):
-        return f"<Department {self.id}: {self.name}, {self.location}>"
+        return f"<Department id={self.id} name={self.name} location={self.location}>"
+
+    @classmethod
+    def create_table(cls):
+        CURSOR.execute('''
+            CREATE TABLE IF NOT EXISTS departments (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                location TEXT
+            )
+        ''')
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        CURSOR.execute('DROP TABLE IF EXISTS departments')
+        CONN.commit()
+
+    def save(self):
+        if self.id is None:
+            CURSOR.execute('INSERT INTO departments (name, location) VALUES (?, ?)', (self.name, self.location))
+            self.id = CURSOR.lastrowid
+        else:
+            self.update()
+        CONN.commit()
+
+    @classmethod
+    def create(cls, name, location):
+        dept = cls(name, location)
+        dept.save()
+        return dept
+
+    def update(self):
+        CURSOR.execute(
+            'UPDATE departments SET name = ?, location = ? WHERE id = ?',
+            (self.name, self.location, self.id)
+        )
+        CONN.commit()
+
+    def delete(self):
+        CURSOR.execute('DELETE FROM departments WHERE id = ?', (self.id,))
+        CONN.commit()
+        self.id = None
+
+    @classmethod
+    def find_by_id(cls, id):
+        CURSOR.execute('SELECT * FROM departments WHERE id = ?', (id,))
+        row = CURSOR.fetchone()
+        if row:
+            return cls(id=row[0], name=row[1], location=row[2])
+        return None
+
+    @classmethod
+    def get_all(cls):
+        CURSOR.execute('SELECT * FROM departments')
+        rows = CURSOR.fetchall()
+        return [cls(id=row[0], name=row[1], location=row[2]) for row in rows]
